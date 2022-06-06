2095feba5038ecc378c319b1b0de64377337a928
Jackson McKay <jay.mckay@utah.edu>
2022-06-06 13:04:06 -0600

    Now using requests.Session()

--- redfish.py
+++ redfish.py
@@ -1,18 +1,6 @@
 import requests
 import sys
-import time
-import re
-from requests.auth import HTTPBasicAuth
-from requests.packages import urllib3
-from requests.exceptions import ReadTimeout, HTTPError, ConnectionError
 from argparse import ArgumentParser
-
-class Connection:
-    def __init__(self, remote, user, password):
-        self.base = "https://" + remote
-        self.auth = HTTPBasicAuth(user, password)
-    def request(self, path):
-        return requests.get(self.base + path, auth=self.auth, verify=False, timeout=15)
             
 class Entry:
     def __init__(self, data):
@@ -29,88 +17,82 @@ class Entry:
 
 def parse_args():
     parser = ArgumentParser(description="SEL collector via Redfish")
-    mandatory = parser.add_argument_group(title="mandatory arguments")
-    mandatory.add_argument("-r", "--remote", help="the remote bmc to query", required=True)
-    mandatory.add_argument("-u", "--user", help="the login username", required=True)
-    mandatory.add_argument("-p", "--password", help="the login password", required=True)
-    mandatory.add_argument("-e", "--events", help="show events only", action='store_true', required=False)
+    arguments = parser.add_argument_group(title="mandatory arguments")
+    arguments.add_argument("-r", "--remote", help="the remote bmc to query", required=True)
+    arguments.add_argument("-u", "--user", help="the login username", required=True)
+    arguments.add_argument("-p", "--password", help="the login password", required=True)
     args = parser.parse_args()
     return args
 
 def query_node(remote, user, password):
 
-    # initial connection information 
-    c = Connection(remote, user, password)
+    root = 'https://' + remote + '/redfish/v1/'
+    session = requests.Session()
+    session.auth = (user, password)
     
-    # find the location of the system managers
-    response = c.request("/redfish/v1/Managers/")
-    response.raise_for_status()
-    data = response.json()
-    managers = [m.get("@odata.id") for m in data.get("Members")]
+    # find paths to system managers
+    response = session.get(root + 'Managers/')
+    if not response.ok:
+        print("No path to systems: " + response.reason)
+    else:
+        data = response.json()
+        managers = [m.get("@odata.id") for m in data.get("Members")]
 
-    # find the location of the system
-    response = c.request("/redfish/v1/Systems/")
-    response.raise_for_status()
-    data = response.json()
-    systems = [s.get("@odata.id") for s in data.get("Members")]
+    # find paths to systems
+    response = session.get(root + 'Systems/')
+    if not response.ok:
+        print("No path to managers:" + response.reason)
+    else:
+        data = response.json()
+        systems = [s.get("@odata.id") for s in data.get("Members")]
 
-    # for each manager, find the log services attached to it
+    if not managers and not systems:
+        print("Could not collect events")
+        sys.exit()
+
     services = []
-    for m in managers:
-        response = c.request(m).json()
-        location = response.get("LogServices").get("@odata.id")
-        response = c.request(location).json()
+    # on each manager and system page, find the path to log services
+    for page in managers + systems:
+        response = session.get(page)
+        data = response.json()
+        location = data.get("LogServices").get("@odata.id")
+        response = session.get(location).json()
         members = [s.get("@odata.id") for s in response.get("Members")]
         services.extend(members)
     
-    # for each system, see if there exists log services attached to it
-    for m in systems:
-        response = c.request(m).json()
-        location = response.get("LogServices")
-        if location is not None:
-            location = location.get("@odata.id")
-            response = c.request(location).json()
-            members = [s.get("@odata.id") for s in response.get("Members")]
-            services.extend(members)
-
-
     # for each log service, find log locations
-    logs = []
-    for s in services:
-        response = c.request(s).json()
-        log = response.get("Entries")
-        if log is not None:
-            log = log.get("@odata.id")
-            logs.append(log)
+    # logs = []
+    # for s in services:
+    #     response = c.request(s).json()
+    #     log = response.get("Entries")
+    #     if log is not None:
+    #         log = log.get("@odata.id")
+    #         logs.append(log)
 
-    # for each log, get all entries
-    entries = []
-    for l in logs:
-        response = c.request(l).json()
-        members = response.get("Members")
-        for m in members:
-            e = Entry(m)
-            if e.severity != "OK":
-                entries.append(e)
+    # for each log, get all entries of interest
+    # entries = []
+    # for l in logs:
+    #     response = c.request(l).json()
+    #     members = response.get("Members")
+    #     for m in members:
+    #         e = Entry(m)
+    #         if e.severity != "OK":
+    #             entries.append(e)
 
-    # NOTE: if we want to "deassert" some of the messages, keep a dict with
-    # the sensor numbers as keys
+    # # NOTE: if we want to "deassert" some of the messages, keep a dict with
+    # # the sensor numbers as keys
             
-    # return our list of errors sorted by newest date
-    return sorted(entries, key=lambda e: e.date, reverse=True)
+    # # return our list of errors sorted by newest date
+    # return sorted(entries, key=lambda e: e.date, reverse=True)
+    
+    session.close()
+    return services
 
 def main():
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     args = parse_args()
-    try:
-        warnings = query_node(args.remote, args.user, args.password)
-        for w in warnings:
-            if args.events:
-                print(w) 
-            else:
-                print(w)
-    except (ReadTimeout, HTTPError, ConnectionError) as e:
-        print(e)
+    warnings = query_node(args.remote, args.user, args.password)
+    for w in warnings:
+        print(w)
             
 if __name__ == "__main__":
     main()           
