a1f440bd90f8550f03d69d2f2ff75d4b709fe81e
Jackson McKay <jay.mckay@utah.edu>
2022-08-10 12:06:03 -0600

    Added additional methods.

--- red.py
+++ red.py
@@ -3,10 +3,7 @@ from argparse import ArgumentParser
 
 class RedfishConnection():
     def __init__(self, remote, user, password):
-        self.remote = remote
-        self.password = password
-        self.user = user
-        self.root = 'https://' + remote + '/redfish/v1/'
+        self.root = 'https://' + remote
         self.session = requests.sessions.Session()
         self.session.auth = (user, password)
         self.session.verify = False
@@ -16,11 +13,28 @@ class RedfishConnection():
         if not response.ok:
             return "unable to fetch from " + resource
         return response.json()
-        
+
+def get_manager_addresses(redfishconnection):
+    response = redfishconnection.get('/redfish/v1/Managers/')
+    addresses = [m.get('@odata.id') for m in response.get('Members')]
+    return addresses
+
+def get_service_address(redfishconnection, manager_location):
+    response = redfishconnection.get(manager_location)
+    address = response.get('LogServices').get('@odata.id')
+    return address
+
+def get_log_addresses(redfishconnection, service_location):
+    response = redfishconnection.get(service_location)
+    addresses = [s.get('@data.id') for s in response.get('Members')]
+
 def query(remote, user, password):
     redfish = RedfishConnection(remote, user, password)
-    data = redfish.get('Managers/')
-    managers = [m.get('@odata.id') for m in data.get('Members')]
+    manager_addresses = get_manager_addresses(redfish)
+    service_addresses = [get_service_address(redfish, address) for address in manager_addresses]
+    log_addresses = [get_log_addresses(redfish, address) for address in service_addresses]
+    for l in log_addresses:
+        print(l)
 
 def parse_args():
     parser = ArgumentParser(description="SEL collector via Redfish")
@@ -35,6 +49,6 @@ def main():
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
     args = parse_args()
     query(args.remote, args.user, args.password)
-            
+
 if __name__ == "__main__":
-    main()           
+    main()
